e1decdbfcdbe7b9a9ea68487544737a27f0477a8
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2022-11-30 12:50:07 -0500

    added comment routes and index

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const catchRoutes = require('./catchRoutes');
+
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
-router.use('/catches', catchRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
